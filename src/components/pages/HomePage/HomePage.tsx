ac51eb77b58815e925a3748228e640aa10919001
koki-develop <kou.pg.0131@gmail.com>
2023-06-04 11:20:03 +0900

    yarn fmt

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-  return (
-    <div>hello world</div>
-  )
-}
+  return <div>hello world</div>;
+};
 
 export default HomePage;
